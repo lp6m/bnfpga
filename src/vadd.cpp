9113382d159dc06b607044886b6e74a90a01af1a
nittax <nittax>
2020-01-18 17:30:49 +0900

    n=10 System build successfully

--- src/vadd.cpp
+++ src/vadd.cpp
@@ -9,7 +9,7 @@
 #include <ap_fixed.h>
 
 #define NUMOF_DATASETS 1000
-
+#define MAXOF_VARS 10
 #define DATA_BIT 8
 typedef ap_uint<DATA_BIT> data_t;
 static const std::string error_message =
@@ -18,9 +18,9 @@ static const std::string error_message =
 
 using namespace std;
 
-void load_data(int NUMOF_VARS, ap_uint<32> data[NUMOF_DATASETS]){
+void load_data(int NUMOF_VARS, ap_uint<32> data[NUMOF_DATASETS], string filepath){
 	ifstream ifs;
-	ifs.open("../asia10.idt", std::ios::in);
+	ifs.open(filepath, std::ios::in);
 	if (!ifs) {
 	  cerr << "training file open failed" << endl;
 	}
@@ -46,7 +46,6 @@ void load_data(int NUMOF_VARS, ap_uint<32> data[NUMOF_DATASETS]){
 	ifs.close();
 }
 int main(int argc, char* argv[]) {
-	//cout << fixed << setprecision(10) << hls::lgamma(1.0) <<" " << hls::lgamma(1.0)<< endl;
     //TARGET_DEVICE macro needs to be passed from gcc command line
     if(argc != 2) {
     std::cout << "Usage: " << argv[0] <<" <xclbin>" << std::endl;
@@ -109,18 +108,15 @@ int main(int argc, char* argv[]) {
 
     // This call will get the kernel object from program. A kernel is an
     // OpenCL function that is executed on the FPGA.
-    // cl::Kernel krnl_vector_add(program,"krnl_vadd");
-    //cl::Kernel krnl(program,"dp_bayesian_learning");
     cl::Kernel krnl(program,"hypercube_kernel");
 
     // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device.
-    // size_t size_in_bytes = DATA_SIZE * sizeof(int);
+    int NUMOF_VARS = 10;
     cl::Buffer buffer_nof_vars(context, CL_MEM_READ_ONLY, 1 * sizeof(int));
     cl::Buffer buffer_dataset(context, CL_MEM_READ_ONLY, NUMOF_DATASETS * sizeof(ap_uint<32>));
     cl::Buffer buffer_max_vals(context, CL_MEM_READ_ONLY, sizeof(ap_uint<32>));
-    //cl::Buffer buffer_adjacent_matrix(context, CL_MEM_WRITE_ONLY, NUMOF_VARS * NUMOF_VARS * sizeof(int));
-    //cl::Buffer buffer_best_order(context, CL_MEM_WRITE_ONLY, NUMOF_VARS * sizeof(int));
+    cl::Buffer buffer_best_matrix(context, CL_MEM_WRITE_ONLY, MAXOF_VARS / 2 * sizeof(ap_uint<32>));
     cl::Buffer buffer_best_score(context, CL_MEM_WRITE_ONLY, 1 * sizeof(float));
     //set the kernel Arguments
     int narg=0;
@@ -128,20 +124,18 @@ int main(int argc, char* argv[]) {
     krnl.setArg(narg++, buffer_dataset);
     krnl.setArg(narg++, buffer_max_vals);
     krnl.setArg(narg++, buffer_best_score);
-    /*krnl.setArg(narg++, buffer_adjacent_matrix);
-    krnl.setArg(narg++, buffer_best_order);}*/
+    krnl.setArg(narg++, buffer_best_matrix);
 
     //We then need to map our OpenCL buffers to get the pointers
     int *ptr_nof_vars = (int*)q.enqueueMapBuffer(buffer_nof_vars, CL_TRUE, CL_MAP_READ, 0, 1 * sizeof(int));
     ap_uint<32> *ptr_dataset = (ap_uint<32>*) q.enqueueMapBuffer (buffer_dataset , CL_TRUE , CL_MAP_READ , 0, NUMOF_DATASETS * sizeof(ap_uint<32>));
     ap_uint<32> *ptr_max_vals = (ap_uint<32>*) q.enqueueMapBuffer (buffer_max_vals , CL_TRUE , CL_MAP_READ , 0, sizeof(ap_uint<32>));
-    //int *ptr_adjacent_matrix = (int *) q.enqueueMapBuffer (buffer_adjacent_matrix , CL_TRUE , CL_MAP_WRITE , 0, NUMOF_VARS * NUMOF_VARS * sizeof(int));
-    //int *ptr_best_order = (int *) q.enqueueMapBuffer (buffer_best_order , CL_TRUE , CL_MAP_WRITE , 0, NUMOF_VARS * sizeof(int));
+    ap_uint<32> *ptr_best_matrix = (ap_uint<32>*) q.enqueueMapBuffer (buffer_best_matrix , CL_TRUE , CL_MAP_WRITE , 0, MAXOF_VARS / 2 * sizeof(ap_uint<32>));
     float *ptr_best_score = (float *)q.enqueueMapBuffer (buffer_best_score , CL_TRUE , CL_MAP_WRITE , 0, 1 * sizeof(float));
-    int NUMOF_VARS = 10;
+
     *ptr_nof_vars = NUMOF_VARS;
     //load dataset file
-    load_data(NUMOF_VARS, ptr_dataset);
+    load_data(NUMOF_VARS, ptr_dataset, "../asia10.idt");
     //setting input data
     *ptr_max_vals = 0;
     for(int i = 0; i < NUMOF_VARS; i++){
@@ -149,9 +143,6 @@ int main(int argc, char* argv[]) {
     	*ptr_max_vals |= (max_val << i*2);
     }
     //for(int i = 0; i < NUMOF_VARS*NUMOF_VARS; i++) ptr_adjacent_matrix[i] = 0;
-
-
-
 
     // Data will be migrated to kernel space
     q.enqueueMigrateMemObjects({buffer_dataset, buffer_max_vals},0/* 0 means from host*/);
@@ -164,8 +155,7 @@ int main(int argc, char* argv[]) {
     // The result of the previous kernel execution will need to be retrieved in
     // order to view the results. This call will transfer the data from FPGA to
     // source_results vector
-    //q.enqueueMigrateMemObjects({buffer_adjacent_matrix, buffer_best_order},CL_MIGRATE_MEM_OBJECT_HOST);
-    q.enqueueMigrateMemObjects({buffer_best_score},CL_MIGRATE_MEM_OBJECT_HOST);
+    q.enqueueMigrateMemObjects({buffer_best_score, buffer_best_matrix},CL_MIGRATE_MEM_OBJECT_HOST);
 
     q.finish();
     t4 = std::chrono::system_clock::now();
@@ -181,6 +171,23 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }*/
     cout << "best_score : " << *ptr_best_score << endl;
+    cout << "best_matrix" << endl;
+    for(int i = 0; i < NUMOF_VARS/2; i++){
+    	ap_uint<16> firstdata = ptr_best_matrix[i] >> 16;
+    	ap_uint<16> seconddata = ptr_best_matrix[i].range(15, 0);
+    	for(int j = 0; j < NUMOF_VARS; j++){
+    		bool elm = (firstdata >> j & 1U);
+    		cout << elm << " ";
+    	}
+    	cout << endl;
+    	if(i * 2 + 1 < NUMOF_VARS){
+				for(int j = 0; j < NUMOF_VARS; j++){
+					bool elm = (seconddata >> j & 1U);
+					cout << elm << " ";
+				}
+				cout << endl;
+    	}
+    }
     double elapsed1 = (double)std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
     double elapsed2 = (double)std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count();
     double elapsed3 = (double)std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count();
@@ -192,8 +199,7 @@ int main(int argc, char* argv[]) {
     q.enqueueUnmapMemObject(buffer_dataset , ptr_dataset);
     q.enqueueUnmapMemObject(buffer_max_vals ,ptr_max_vals);
     q.enqueueUnmapMemObject(buffer_best_score ,ptr_best_score);
-    //q.enqueueUnmapMemObject(buffer_adjacent_matrix ,ptr_adjacent_matrix);
-    //q.enqueueUnmapMemObject(buffer_best_order, ptr_best_order);
+    q.enqueueUnmapMemObject(buffer_best_matrix ,ptr_best_matrix);
 
     q.finish();
 
